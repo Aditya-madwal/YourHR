bbc09c4b754dd8f06721b9cae2ed0f32f3a4b74a
Aditya-madwal <madwaladitya@gmail.com>
2024-08-30 15:57:58 +0530

    UI changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
       <div className="mx-auto max-w-lg text-center">
         <h1 className="text-2xl font-bold sm:text-3xl">Login to FriendsBook</h1>
 
-        <p className="mt-4 text-gray-500 italic">"Connect, Share, Thrive!"</p>
+        <p className="mt-4 text-gray-500 italic">Login to your account!</p>
       </div>
 
       <form
@@ -77,21 +77,7 @@ const Login = () => {
               }}
             />
 
-            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                />
-              </svg>
-            </span>
+            <span className="absolute inset-y-0 end-0 grid place-content-center px-4"></span>
           </div>
         </div>
 
@@ -113,27 +99,7 @@ const Login = () => {
               }}
             />
 
-            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                />
-              </svg>
-            </span>
+            <span className="absolute inset-y-0 end-0 grid place-content-center px-4"></span>
           </div>
         </div>
 
--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -13,17 +13,6 @@ const Page404 = () => {
           </p>
 
           <p className="mt-4 text-gray-500">We can't find that page.</p>
-
-          <Link
-            to="/"
-            className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring mr-3">
-            Go Back Home
-          </Link>
-          <a
-            href={currentUrl}
-            className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring">
-            Retry
-          </a>
         </div>
       </div>
     </div>
